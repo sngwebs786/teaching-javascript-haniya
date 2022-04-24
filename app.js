e1f9d340f1a90c9870483e852b3ceca2dc491a47
syedanashraghaffar <work.nashra@gmail.com>
2022-04-24 23:39:18 +0500

    1st class code pushed

---
+++ app.js
@@ -0,0 +1,42 @@
+// ============================================== 1st class of JS 
+
+// variable ( temporary storage )
+// vary (change)
+// declare/declaration
+// keywords(pre-defined words/terms)
+// statement terminator ;
+// Assignment Operator (=)
+// parenthesis() 
+// braces {}
+
+//developer
+//user 
+
+
+// DECLARATION OF VARIABLE
+
+// keyword variablename
+var abc; //declare
+abc = 5 //value assign
+
+// INITIALIZATION OF VARIABLE
+
+var xyz = 4 //variable initialization
+
+xyz = 5
+
+
+// 1. Input 
+
+// => prompt 
+var age = prompt("Enter your age")
+
+// 2. Output 
+
+// => alert
+// => console.log
+// => document.write
+
+alert(age)
+
+
