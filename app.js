4753782b296c84755099e04969f32c0acfb70c32
syedanashraghaffar <work.nashra@gmail.com>
2022-04-30 22:55:29 +0500

    Class 2 code

--- app.js
+++ app.js
@@ -16,20 +16,20 @@
 // DECLARATION OF VARIABLE
 
 // keyword variablename
-var abc; //declare
-abc = 5 //value assign
+// var abc; //declare
+// abc = 5 //value assign
 
 // INITIALIZATION OF VARIABLE
 
-var xyz = 4 //variable initialization
+// var xyz = 4 //variable initialization
 
-xyz = 5
+// xyz = 5
 
 
 // 1. Input 
 
 // => prompt 
-var age = prompt("Enter your age")
+// var age = prompt("Enter your age","My age is")
 
 // 2. Output 
 
@@ -37,6 +37,59 @@ var age = prompt("Enter your age")
 // => console.log
 // => document.write
 
-alert(age)
+// alert(age)
+// alert("Hello") (string)
+
+// ============================================== 2nd class of JS 
+
+// Data types 
+// JAVASCRIPT
+// 1. Number (1,2,3,1.5,2.5)
+// 2. String ("sjddska543743") (constant)
+// 3. Boolean (false,true)
+
+// OTHER LANGUAGES
+// character ('A') 
+//  integer(1,2)
+// float (3.4)
+// double 
+
+
+// Alert 
+
+// var a = 5+2;
+// alert("Haniya")
+// alert(a)
+// alert(8)
+// alert(2+2)
+
+// var b=3;
+// alert(b+2)
+
+// Concatenation
+// alert("Haniya" + 5) /Haniya5
+
+
+// prompt takes by default String values
+// var userInput = prompt("Enter any number");
+// alert(userInput+1)
+
+// Taking Number as Input 
+
+// var userInput = +prompt("Enter any number")
+// alert(userInput+2);
+
+
+// => console.log
+
+// Developers
+// console.log("Hello")
+
+// var a =4;
+// console.log(a+2)
+
+// => Document.write
+
+// document.write("Hello")
 
 
